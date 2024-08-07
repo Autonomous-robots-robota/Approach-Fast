aa2b733d7f2c974578be514e2cb62f0ae9345af9
Yehudit-Brickner <nech.diti@yahoo.com>
2024-08-07 11:24:51 +0300

    added funcs

--- scenario.py
+++ scenario.py
@@ -13,6 +13,36 @@ class Scenario:
                               self.config.pre_dist)
         # self.dronePath = self.case.dronePath_array
         self.x_points = np.linspace(0, self.config.intercept_point[0], 100)
+        self.speed_param = 30  # will be in the config csv
+        self.speed = 0
+
+    def update_speed(self, time_from_start):
+        # calculate the speed of the drone, m/s
+        # the speed linearly goes up to speed_param and gets to that speed at 1 sec
+        # and then stays steady
+        if time_from_start < 1:
+            self.speed = time_from_start * self.speed_param
+        else:
+            self.speed = self.speed_param
+
+    def calc_dist(self, last_dist, time_from_start_last, dx):
+        # calc the distance the drone has moved
+        # take the last distance + the time difference * the average speed
+        old_speed = self.speed
+        self.update_speed(time_from_start_last + dx)
+        return last_dist + dx * ((old_speed + self.speed) / 2)
+
+    def curve(self, p0, p1, p2, pcurr, P = None):
+        # this function calcs the ratio from curr to p1 and curr to p2,
+        # if its smaller then P then continue aiming for p1,
+        # if its bigger then P then update p0 = p1, p1 = p2, p2 = new_p
+        if P is None:
+            P = self.distance(p0[0], p0[1], p1[0], p1[0])/self.distance(p0[0], p0[1], p2[0], p2[0])
+        curr_ratio = self.distance(pcurr[0], pcurr[1], p1[0], p1[0])/self.distance(pcurr[0], pcurr[1], p2[0], p2[0])
+        if curr_ratio > P:
+            print("update points")
+        else:
+            return curr_ratio
 
     def update(self, old_point):
         """
@@ -32,6 +62,7 @@ class Scenario:
         return x
 
     def find_spot_on_parabulah(self, a, b, c, drone_pos_curr, drone_start_pos, intercept_pos):
+        # this function uses binary search to find the closest spot on the parabulah
         if drone_start_pos[0] <= intercept_pos[0]:
             x1 = drone_start_pos[0]
             x2 = intercept_pos[0]
@@ -56,9 +87,12 @@ class Scenario:
         return (x1 + x2) / 2, (y1 + y2) / 2
 
     def distance(self, x1, y1, x2, y2):
+        # distace between 2 points
         return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
     def find_spot_on_linear(self, m, n, drone_pos_curr, drone_start_pos, intercept_pos):
+        # find the closest spot on a linear line using binary search
+        # most likely use the other search - "find_spot+on_linear2"
         if drone_start_pos[0] <= intercept_pos[0]:
             x1 = drone_start_pos[0]
             x2 = intercept_pos[0]
@@ -83,4 +117,6 @@ class Scenario:
         return (x1 + x2) / 2, (z1 + z2) / 2
 
     def find_spot_on_linear2(self, m, n, curr_x):
+        # this function gets the correct x from the fixed parbulah,
+        # with the correct x it gets the y
         return m * curr_x + n
